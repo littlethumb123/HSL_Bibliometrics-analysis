7574a77c4cdafeaca876ada29de5aafb7fd3a980
ZX_UNC <42219594+littlethumb123@users.noreply.github.com>
2019-12-14 20:24:49 -0500

    Update Fei's Dec affiliation detection tasks.py

--- Fei's Dec affiliation detection tasks.py
+++ Fei's Dec affiliation detection tasks.py
@@ -19,51 +19,67 @@ target_uni = ["Johns Hopkins University",
 pubmed_dic  = pp.parse_medline_xml(path, author_list=True)  # here path is the xml file path, a placeholder here
 #print(json.dumps(pubmed_dic, indent = 4))  #explore the parsed data if needed
 
-"""
+'''
 Basic idea:
 Medline xml in, through pmc search and process pmc_oa_xml
 
 12/7: 
 aff reference id is not consistent and all pmcids are not captured 
 Here show two types of oa xml formats https://www.ncbi.nlm.nih.gov/pmc/pmcdoc/tagging-guidelines/article/style.html
-"""
 
-miss_affi_article_info = []
-target_affi_article_info = []
+12/14 updates: just add the missing authors
+remove ('forename': '', 'firstname': '', 'lastname': '', 'affiliation': '')
+
+'''
+miss_affi_article_info = []  
+target_affi_article_info = {key:[] for key in target_uni} # add target affiliation as key and 
 artcile_count = len(pubmed_dic)
+
+# code testing data
+complete_add_article = []
+fail_add_article = []
+miss_article_before_add = set()
 for article in pubmed_dic: # a list of article info
     pmid = article['pmid']
     doi = article['doi']  # Could be empty
     pmcid = article['pmc_id']  # Could be empty
     #first_author_affiliation = article['authors'][0]['affiliation']
-    i_miss_affi = 0
     target_affi = []
+    article_affi = []  # particularly for task2 to collect affiliation information list
     for author in article['authors']:
-        # Task_1
-        if author['affiliation'] == '':
-            i_miss_affi = i_miss_affi+1
-        # Task_2  better separate from task 1 
-        if any(j in author['affiliation'] for j in target_uni):
-            target_affi.append(author['affiliation'])
-    if i_miss_affi>0:
-        print("here lacks ", i_miss_affi)
-        if pmcid!='':
-            article['authors'] = retrieve_affil(pmcid) # here do the search and return affiliation
-            print("can add and add the following affiliation: ", article['authors'])
+        if author['lastname']!="" and author['forename']!="":
+            article_affi.append(author['affiliation'])
         else:
-            print("cannot add because pmcid is empty")
-            miss_affi_article_info.append((pmid, doi, 'first_author_affiliation'))   
-    if len(target_affi)!= 0:
-        target_affi_article_info.append((pmid, doi, target_affi))
+            article['authors'].remove(author)
+    if "" in article_affi:
+        miss_article_before_add.add(pmid)
+        if pmcid!='':
+            article['authors'] = retrieve_affil(pmcid) # here do the search and retreive affiliation information
+            article_affi.clear()
+            article_affi = [author['affiliation'] for author in article['authors']]
+            complete_add_article.append(pmid)
+        else:
+            print("{} cannot perform pmc search because pmcid isnot available".format(pmid))
+            # Task_1
+            fail_add_article.append(pmid)
+            miss_affi_article_info.append((pmid, doi))
+    
+    # Task_2  better separate from task 1 can be performed after affiliation completion
+    for j in target_uni:
+        for i in article_affi:
+            if type(i) == str:  # deal with string type affiliation
+                if j in i and pmid not in target_affi_article_info[j]:
+                    target_affi_article_info[j].append(pmid)
+            else: # deal with list-like affiliation
+                for ii in i:
+                    if j in ii and pmid not in target_affi_article_info[j]:
+                        target_affi_article_info[j].append(pmid)
+                
 
-# export as csv files
-with open('319task.csv','w', encoding='utf-8') as out:
-    csv_out=csv.writer(out)
-    csv_out.writerow(['pubmed_id','doi', 'target_affiliation'])
-    for row in target_affi_article_info:
-        csv_out.writerow(row)
-with open('234task.csv','w', encoding='utf-8') as out:
-    csv_out=csv.writer(out)
-    csv_out.writerow(['pubmed_id','doi', 'first_author affiliation'])
-    for row in miss_affi_article_info:
-        csv_out.writerow(row)
+# # export as csv files
+# with open('319task.csv','w', encoding='utf-8') as out:
+#     csv_out=csv.writer(out)
+#     csv_out.writerow(['pubmed_id','doi', 'target_affiliation'])
+#     for row in target_affi_article_info:
+#         csv_out.writerow(row)
+
